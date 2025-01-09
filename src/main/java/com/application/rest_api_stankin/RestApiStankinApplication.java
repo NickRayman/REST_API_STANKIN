77057bb5378c9fa0a929b355aff0ba33356220df
NickRayman <kolya.chechnev@mail.ru>
2025-01-09 19:23:33 +0500

    Start Project

---
+++ src/main/java/com/application/rest_api_stankin/RestApiStankinApplication.java
@@ -0,0 +1,13 @@
+package com.application.rest_api_stankin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiStankinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiStankinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/rest_api_stankin/RestApiStankinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.rest_api_stankin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiStankinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
