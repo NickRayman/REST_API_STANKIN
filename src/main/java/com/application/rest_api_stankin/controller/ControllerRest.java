9c9326c8dcbafa9132683bbddcfb0bf1cefb38d3
NickRayman <kolya.chechnev@mail.ru>
2025-02-25 02:01:03 +0300

    Подключил UI-документацию Swagger в проект

--- src/main/java/com/application/rest_api_stankin/controller/ControllerRest.java
+++ src/main/java/com/application/rest_api_stankin/controller/ControllerRest.java
@@ -1,53 +1,73 @@
 package com.application.rest_api_stankin.controller;
 
+import com.application.rest_api_stankin.DTO.CatDTO;
 import com.application.rest_api_stankin.entity.Cat;
 import com.application.rest_api_stankin.repository.CatRepo;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
-  Класс rest-точек для взаимодействия с сервисами StankinService
+ * Класс rest-точек для взаимодействия с сервисами StankinService
  */
+@Tag(name = "Cats API", description = "Методы для работы с котами")
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class ControllerRest {
 
     private final CatRepo catRepo;
-    private final ObjectMapper objectMapper;
-
-    public ControllerRest(CatRepo catRepo, ObjectMapper objectMapper) {
-        this.catRepo = catRepo;
-        this.objectMapper = objectMapper;
-    }
 
     /**
      * Endpoint для добавления JSON-записей в таблицу cats
-     * @param cat
+     *
+     * @param catDTO
      */
+    @Operation(
+            summary = "Endpoint для добавления JSON-записей в таблицу cats",
+            description = "Получает DTO кота и билдером собирет и сохраняет сущность в базу"
+    )
     @PostMapping("/api/add")
-    public void addCat(@RequestBody Cat cat) {
-        log.info("New row: {}", catRepo.save(cat));
+    public void addCat(@RequestBody CatDTO catDTO) {
+
+        log.info(
+                "New row: {}", catRepo.save(
+                        Cat.builder()
+                                .age(catDTO.getAge())
+                                .weight(catDTO.getWeight())
+                                .name(catDTO.getName())
+                                .build())
+        );
     }
 
     /**
      * Endpoint для возврата всех полей таблицы cats
+     *
      * @return
      */
+    @Operation(
+            summary = "Endpoint для возврата всех полей таблицы cats",
+            description = "Возвращает список всех котов из базы"
+    )
     @GetMapping("/api/all")
     public List<Cat> getAll() {
-            return catRepo.findAll();
+        return catRepo.findAll();
     }
 
     /**
      * Endpoint для возврата одной записи по id из таблицы cats
+     *
      * @param id
      * @return
      */
+    @Operation(
+            summary = "Endpoint для возврата одной записи по id из таблицы cats",
+            description = "Возвращает кота по id из базы"
+    )
     @GetMapping("/api")
     public Cat getCat(@RequestParam int id) {
         return catRepo.findById(id).orElseThrow();
@@ -55,17 +75,27 @@ public class ControllerRest {
 
     /**
      * Endpoint для удаления одной записи по id из таблицы cats
+     *
      * @param id
      */
+    @Operation(
+            summary = "Endpoint для удаления одной записи по id из таблицы cats",
+            description = "Удаляет запись по id из базы"
+    )
     @DeleteMapping("/api")
     public void deleteCat(@RequestParam int id) {
         catRepo.deleteById(id);
     }
 
     /**
-     * Endpoint для добавления JSON-записей в таблицу cats
+     * Endpoint для редактирования добавленых JSON-записей в таблицу cats
+     *
      * @param cat
      */
+    @Operation(
+            summary = "Endpoint для редактирования добавленых JSON-записей в таблицу cats",
+            description = "Изменяет запись по id в базы"
+    )
     @PutMapping("/api/add")
     public String changeCat(@RequestBody Cat cat) {
         if (!catRepo.existsById(cat.getId()))
@@ -73,4 +103,4 @@ public class ControllerRest {
         return catRepo.save(cat).toString();
     }
 
- }
+}
