c4cf6f18fb44cdc2ac85af6eebc54ad5bd08e53e
NickRayman <kolya.chechnev@mail.ru>
2025-02-24 20:53:14 +0300

    Настроил lombok и создал две rest точки

--- src/main/java/com/application/rest_api_stankin/controller/ControllerRest.java
+++ src/main/java/com/application/rest_api_stankin/controller/ControllerRest.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -27,7 +29,18 @@ public class ControllerRest {
 
     @GetMapping("/api/cat")
     public String giveCat() {
-        Cat cat = new Cat("", 1, 1);
+        Cat cat = new Cat("Barsik", 5, 10);
+        String jsonData = null;
+        try {
+            jsonData = objectMapper.writeValueAsString(cat);
+        } catch (JsonProcessingException e) {
+            System.out.println("Error with cat");
+        }
+        return jsonData;
+    }
+    @PostMapping("/api/special")
+    public String giveSpecialCat(@RequestParam String name) {
+        Cat cat = new Cat(name, 5, 10);
         String jsonData = null;
         try {
             jsonData = objectMapper.writeValueAsString(cat);
